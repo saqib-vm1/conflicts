0ddd6a22c38ccc2bb6aee0d91e44f23c45d75e76
Mohd Saqib <mohd.saqib@vidyamantra.com>
2023-01-13 10:47:00 +0530

    feature divide added

--- common-file.ts
+++ common-file.ts
@@ -4,3 +4,7 @@ function subtraction(a: number, b:number): number {
 function addition(a: number, b: number): number {
     return a + b;
 }
+// here return type is inferred by typescript
+function divide(a: number, b:number) {
+    return a / b;
+}
